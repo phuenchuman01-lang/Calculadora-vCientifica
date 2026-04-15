7a3eb0e6d5a5aeda9f79191f9347097892071c8e
Pato <p.huenchuman01@ufromail.cl>
2026-04-15 00:14:20 -0400

    Refactor calcularRaices method in EcuacionCuadratica

--- src/EcuacionCuadratica.java
+++ src/EcuacionCuadratica.java
@@ -1,10 +1,9 @@
-public class EcuacionCuadratica {
+public class EcuacionCuadratica {    //Calculo de Ecuaciones Cuadraticas y definiciones en el discriminante
 
-    // Ahora devuelve un arreglo de decimales (double[])
     public double[] calcularRaices(double a, double b, double c) {
         if (a == 0) {
             System.out.println("[Error] Si 'A' es 0, no es una ecuación cuadrática.");
-            return new double[]{}; // Devuelve vacío
+            return new double[]{};
         }
 
         double discriminante = Math.pow(b, 2) - (4 * a * c);
@@ -14,16 +13,16 @@ public class EcuacionCuadratica {
             double x1 = (-b + Math.sqrt(discriminante)) / (2 * a);
             double x2 = (-b - Math.sqrt(discriminante)) / (2 * a);
             System.out.println("x1 = " + x1 + " | x2 = " + x2);
-            return new double[]{x1, x2}; // Devuelve las dos raíces
+            return new double[]{x1, x2};
 
         } else if (discriminante == 0) {
             double x = -b / (2 * a);
             System.out.println("x = " + x);
-            return new double[]{x}; // Devuelve una raíz
+            return new double[]{x};
 
         } else {
             System.out.println("Las soluciones son números complejos (imaginarios).");
-            return new double[]{}; // Devuelve vacío porque no son reales
+            return new double[]{};
         }
     }
 }
